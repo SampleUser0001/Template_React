cd183a60b6f395c6f44ab96d8a31ca488cd83d6b
SampleUser0001 <ittimfn+github@gmail.com>
2024-01-15 22:56:56 +0900

    Feature/init (#1)
    * init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Template_React
+
 React.jsのテンプレート
+
+- [Template\_React](#template_react)
+  - [起動](#起動)
+
+## 起動
+
+```bash
+npm run build
+npm run start
+```
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "dist", "asserts"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: (/node_modules/),
+            loader: "babel-loader"
+        }]
+    },
+    devtool: "source-map"
+};
